933c0ddc5d9af7434c60f49c9acc9a719228176c
Joao Patricio <ijpatricio@gmail.com>
2023-12-22 12:30:03 +0000

    Add GitPod.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import laravel from 'laravel-vite-plugin'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const extendedViteDevServerOptions = {}
+
+if (process.env.GITPOD_VITE_URL) {
+    extendedViteDevServerOptions.hmr = {
+        protocol: 'wss',
+        host: new URL(process.env.GITPOD_VITE_URL).hostname,
+        clientPort: 443
+    }
+}
 
 export default defineConfig({
+    server: {
+        ...extendedViteDevServerOptions
+    },
     plugins: [
         laravel({
-            input: ['resources/sass/app.scss', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
     ],
